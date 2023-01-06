7b627925315e638c27888cb7d397cd278be40999
timrolsh <tim.rolsh@gmail.com>
2023-01-06 00:35:37 -0500

    clean up project/homepage

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-/*
-This JS will run on every single page of the website
-*/
-
-const wideRoot = document.getElementById("wide");
+/*This JS will run on every single page of the website*/ const wideRoot = document.getElementById("wide");
 const narrowRoot = document.getElementById("narrow");
 function resize() {
     if (window.outerWidth <= 756) {
@@ -13,19 +9,14 @@ function resize() {
         narrowRoot.setAttribute("style", "display: none;");
     }
 }
-
 resize();
-
 let anchors = document.getElementsByClassName("link");
-
 for (let a = 0; a < anchors.length; ++a) {
     if (anchors[a].getAttribute("href") === "") {
         anchors[a].setAttribute("style", "text-decoration: underline;");
     }
 }
-
 setInterval(resize, 1);
-
 const links = document.getElementById("links-narrow");
 document.getElementById("narrow").addEventListener("click", () => {
     if (links.getAttribute("style")) {
