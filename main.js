30155df15c2f417aedc08dbe66864eca92c3a447
Tim Rolshud <68395794+timrolsh@users.noreply.github.com>
2023-01-06 00:36:53 -0500

    add page header, page content, page styling

---
+++ main.js
@@ -0,0 +1,27 @@
+/*This JS will run on every single page of the website*/ const wideRoot = document.getElementById("wide");
+const narrowRoot = document.getElementById("narrow");
+function resize() {
+    if (window.outerWidth <= 756) {
+        narrowRoot.removeAttribute("style");
+        wideRoot.setAttribute("style", "display: none;");
+    } else {
+        wideRoot.removeAttribute("style");
+        narrowRoot.setAttribute("style", "display: none;");
+    }
+}
+resize();
+let anchors = document.getElementsByClassName("link");
+for (let a = 0; a < anchors.length; ++a) {
+    if (anchors[a].getAttribute("href") === "") {
+        anchors[a].setAttribute("style", "text-decoration: underline;");
+    }
+}
+setInterval(resize, 1);
+const links = document.getElementById("links-narrow");
+document.getElementById("narrow").addEventListener("click", () => {
+    if (links.getAttribute("style")) {
+        links.removeAttribute("style");
+    } else {
+        links.setAttribute("style", "display: none;");
+    }
+});
